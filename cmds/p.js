5b999bf093cde795a9cee09038110ab63aed158b
! Yusuf <61831457+Yusuf-AS@users.noreply.github.com>
2020-09-21 14:17:52 +0700

    Create p.js

---
+++ cmds/p.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js")
+
+module.exports.run = async (client, message, args) => {
+    if (message.author.id !== "546305510865895435") return;
+  let sayang = args.join(" ");
+    if (!args[0]) return message.reply("Please enter the word!").then(msg => msg.delete(4000));
+
+  const cmd = args.join(" ").split(" | ")
+
+  message.channel.send(`owopray ${sayang}`).then(msg => msg.delete(1000))
+  message.delete()
+  };
+
+module.exports.help = {
+        name: "s"
+      }
