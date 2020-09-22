e86be51290f5e46141b1240e34fec12b0807a9a3
! Yusuf <61831457+Yusuf-AS@users.noreply.github.com>
2020-09-23 05:53:44 +0700

    Update s.js

--- cmds/s.js
+++ cmds/s.js
@@ -3,7 +3,7 @@ const Discord = require("discord.js")
 module.exports.run = async (client, message, args) => {
     if (message.author.id !== "546305510865895435") return;
   let sayang = args.join(" ");
-    if (!args[0]) return message.reply("Please enter the word!").then(msg => msg.delete(4000));
+    if (!args[0]) return message.reply("a").then(msg => msg.delete(400));
 
   const cmd = args.join(" ").split(" | ")
 
