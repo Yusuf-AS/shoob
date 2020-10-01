f87f23df6b8e13d1141d534daba43cb0ef993ade
! Yusuf <61831457+Yusuf-AS@users.noreply.github.com>
2020-10-01 07:40:17 +0700

    Update p.js

--- cmds/p.js
+++ cmds/p.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js")
 
 module.exports.run = async (client, message, args) => {
-    if (message.author.id !== "546305510865895435") return;
+    if (message.author.id !== "553455706477428737") return;
   let sayang = args.join(" ");
     if (!args[0]) return;
 
